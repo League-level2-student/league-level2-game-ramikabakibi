0fdaae6d6b03854f85feec4362bbfb8325d38c6e
root <root@eb4f26760fbe>
2020-08-14 00:01:25 +0000

    Finished: Rocket draw and move. Need to make rocket faster and make it so that the rocket is not delayed when moving horizontally. Also need to finish ObjectManager.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,22 +1,32 @@
-import java.awt.Graphics; 
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class GamePanel extends JPanel implements KeyListener {
+public class GamePanel extends JPanel implements KeyListener, ActionListener {
+	final int MENU = 0;
+	final int GAME = 1;
+	final int END = 2;
+	int currentState = MENU;
 	BufferedImage background = null;
+	Rocket rocket;
+	Timer frameDraw;
 
 	GamePanel() {
 		try {
 			background = ImageIO.read(this.getClass().getResourceAsStream("space.jpg"));
+
 		} catch (Exception e) {
 			System.out.println("Could not display background");
 		}
-
-		repaint();
+		rocket = new Rocket(300, 568, 100, 200);
+		frameDraw=new Timer(1000/60, this);
 	}
 
 	@Override
@@ -25,24 +35,43 @@ public class GamePanel extends JPanel implements KeyListener {
 		if (background != null) {
 			g.drawImage(background, 0, 0, Game.WIDTH, Game.HEIGHT, null);
 		}
+		rocket.draw(g);
+		rocket.update();
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-
+		if (currentState == MENU) {
+			if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				rocket.right();
+			}
+			if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+				rocket.left();
+			}
+		}
+		
+		repaint();
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-
+		if(e.getKeyCode()==KeyEvent.VK_RIGHT ||e.getKeyCode()==KeyEvent.VK_LEFT ) {
+			rocket.xspeed=0;
+		}
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		repaint();
 	}
 
 }
--- src/Rocket.java
+++ src/Rocket.java
@@ -1,8 +1,42 @@
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Rocket extends GameObject{
+	int xspeed;
+	BufferedImage rocketship=null;
 	
 	Rocket(int x, int y, int width, int height) {
 		super(x, y, width, height);
+		try {
+			rocketship= ImageIO.read(this.getClass().getResourceAsStream("rocket.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void right() {
+		xspeed = speed;
+	}
+
+	public void left() {
+		xspeed = -speed;
+	}
+	
+	void update() {
+		System.out.println( "1 " + x);
+		if (x + xspeed <= Game.WIDTH - width && x + xspeed >= 0) {
+			System.out.println("2 " + x);
+			x += xspeed;
+		}
+	}
+
+	
+	
+	void draw(Graphics g) {
+		g.drawImage(rocketship, x, y, width, height, null);
 	}
 }
-
